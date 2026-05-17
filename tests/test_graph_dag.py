a1e8496dbdf6163bf9aeeab037e76d22265edb7d
EksanPurnama <eksan0ft.2025@student.uny.ac.id>
2026-05-17 13:09:07 +0700

    Update test_graph_dag.py

--- tests/test_graph_dag.py
+++ tests/test_graph_dag.py
@@ -4,13 +4,21 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.data_structures.bst import BSTMahasiswa 
 from src.data_structures.graph_dag import GraphPrereq 
-from src.modules.modul_1 import ModulTranskrip 
 from src.modules.modul_4 import ModulKurikulum 
 
 GRADE_MAP = {
     "A": 4.0, "A-": 3.7, "B+": 3.3, "B": 3.0, "B-": 2.7,
     "C+": 2.3, "C": 2.0, "D": 1.0, "E": 0.0
 }
+
+class MockNodeNilai:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class MockTranskripsi:
+    def __init__(self):
+        self.head = None
 
 class MockMahasiswa:
     def __init__(self, nim, nama, prodi, angkatan):
@@ -19,7 +27,7 @@ class MockMahasiswa:
         self.prodi = prodi
         self.angkatan = angkatan
         self.ipk = 0.0
-        self.transkripsi = None  
+        self.transkripsi = MockTranskripsi()
 
 class MockNilaiMatkul:
     def __init__(self, kode, nama, sks, grade, semester):
@@ -52,26 +60,23 @@ def test_prasyarat_kurikulum():
     bst = BSTMahasiswa()
     g = GraphPrereq()
     mod4 = ModulKurikulum(g)
-    mod1 = ModulTranskrip()
 
     g.add_edge("INF20112", "INF20214")
 
     nim_test = "21007001"
     m1 = MockMahasiswa(nim_test, "Budi", "INF", 2021)
+
+    n1 = MockNilaiMatkul("INF20112", "Matematika Diskrit", 3, "B", 3)
+    m1.transkripsi.head = MockNodeNilai(n1)
+    
     bst.insert(m1)
     
     node = bst.search(nim_test)
-    
-    n1 = MockNilaiMatkul("INF20112", "Matematika Diskrit", 3, "B", 3)
-    mod1.tambah_entri_nilai(node, n1, GRADE_MAP)
-    
-
     if node:
+        node.transkripsi = m1.transkripsi
         if hasattr(node, 'data') and node.data:
-            node.transkripsi = node.data.transkripsi
-        else:
-            node.transkripsi = m1.transkripsi
-    
+            node.data.transkripsi = m1.transkripsi
+
     bisa_ambil, pesan = mod4.prasyarat_terpenuhi(bst, nim_test, "INF20214", GRADE_MAP)
     
     assert bisa_ambil == True
